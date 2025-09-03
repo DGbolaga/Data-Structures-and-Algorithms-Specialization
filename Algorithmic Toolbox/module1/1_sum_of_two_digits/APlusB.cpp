c258a814f5ccedcc91f28ccc006ea84c2a0fb8dc
gbolaga-daramola <gbolagadaramola765@gmail.com>
2025-09-03 19:05:27 +0100

    Complete assignment solution for module1

---
+++ Algorithmic Toolbox/module1/1_sum_of_two_digits/APlusB.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int sum_of_two_digits(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int a = 0;
+    int b = 0;
+    cin >> a;
+    cin >> b;
+    cout << sum_of_two_digits(a, b) << endl;
+    return 0;
+}
